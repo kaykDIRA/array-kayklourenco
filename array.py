23e3e44c270e5faa9090c70f4d62c81eb14152d9
kaykDIRA <faculdadekayk@gmail.com>
2025-09-01 22:41:09 +0000

    array.py

---
+++ array.py
@@ -0,0 +1,4 @@
+colegas = ["Roni", "david", "amanda", "itamar", "mateus"]
+print("Primeiro elemento:", colegas[0])     # Índice 0
+print("Terceiro elemento:", colegas[2])     # Índice 2
+print("Último elemento:", colegas[4])       # Índice 4
